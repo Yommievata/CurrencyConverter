cdc7e097c1ad472f39ff9db6f3ed90ab3dba2d59
OmotolaniGbodo <olusanyaabayomi@yahoo.com>
2024-02-08 16:23:44 +0100

    Minor fix

--- src/main/java/com/personal/currencyconvata/controller/ConverterController.java
+++ src/main/java/com/personal/currencyconvata/controller/ConverterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class ConverterController {
 
     private final ConverterService converterService;
